d25e85e857566c16f8455c137ca1efd89c2418d1
awasthi <you@example.com>
2021-09-03 20:34:07 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./styles/main.css"
+function App() {
+    return (
+        <>
+                <section>
+                    <div class="container">
+                                   <div class="left"></div>
+                                    <div class="right">
+                                        <div class="content">
+                                            <h1>This is a heading</h1>
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia assumenda dolore reprehenderit ullam blanditiis ex sapiente pariatur tempore incidunt facilis?</p>
+                                            <a href="#" class="btn">Click Me</a>
+                                        </div>
+                                    </div>
+                    </div>
+               </section>  
+        </>
+    )
+}
+
+export default App
